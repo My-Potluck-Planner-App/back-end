dc3e8c96f7630542d6ce7da4d338e02d709fb2f8
Darla Young <me3u2@hotmail.com>
2022-03-02 23:15:16 -0600

    forgot to save server file before last commit

--- api/server.js
+++ api/server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const session = require('express-session');
-const usersRouter = require('./users/users-router');
-const authRouter = require('./auth/auth-router');
-const sessionConfig = require('../config');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
+
 const server = express();
-  
-server.use(session(sessionConfig));
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res) => {
-    res.status(200).json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
 });
 
-server.get('/user', usersRouter);
-server.get('/auth', authRouter);
+server.get("/user", usersRouter);
+server.get("/auth", authRouter);
 
 server.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      message: err.message,
-      stack: err.stack,
-    })
-  })
-  
-  module.exports = server
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
